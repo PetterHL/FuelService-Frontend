cfa9499e527d50c47c128d783b87d4c9f3dc01a5
PetterDesktop <pettersen_97@hotmail.com>
2020-12-02 20:11:36 +0100

    added on click

--- app/src/main/java/com/Fuel/fuelservice/fragment/ForgotPasswordDialogFragment.java
+++ app/src/main/java/com/Fuel/fuelservice/fragment/ForgotPasswordDialogFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +13,22 @@ import androidx.fragment.app.DialogFragment;
 import com.Fuel.fuelservice.R;
 
 public class ForgotPasswordDialogFragment extends DialogFragment {
+    Button button;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.forgotpassord_dialog_fragment, container, false);
+        button = view.findViewById(R.id.forgotPasswordButton);
 
-        
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
         return view;
     }
+
+
 }
